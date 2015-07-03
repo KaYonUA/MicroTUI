65b1078e6481a5555e932736c1d84f3100dbecc6
Vladislav <KaYonUA@users.noreply.github.com>
2015-07-03 11:42:26 +0300

    Edited button render.

--- MicroTUI/Widget/Button.cpp
+++ MicroTUI/Widget/Button.cpp
@@ -13,6 +13,14 @@ namespace MicroTUI
 
 			buffer->Rectangle(_g_widgCord.X, _g_widgCord.Y, WidgetSize.width, WidgetSize.height, Color::Pixel('.', bgL_colr));
 			buffer->Lable(_g_widgCord.X + (WidgetSize.width / 2 - textlen / 2), _g_widgCord.Y + ((WidgetSize.height) / 2), widgettitle, tx_colr);
+			buffer->LineVertical(_g_widgCord.X, _g_widgCord.Y + 1, _g_widgCord.Y + WidgetSize.height - 1, false, tx_colr);
+			buffer->Set(SB_NEWBUFFER, _g_widgCord.X, _g_widgCord.Y, Color::Pixel(TLS_CORNER, tx_colr));
+			buffer->LineHorisontal(_g_widgCord.Y, _g_widgCord.X + 1, _g_widgCord.X + WidgetSize.width - 1, false, tx_colr);
+			buffer->Set(SB_NEWBUFFER, _g_widgCord.X + WidgetSize.width - 1, _g_widgCord.Y, Color::Pixel(TRS_CORNER, tx_colr));
+			buffer->LineVertical(_g_widgCord.X + WidgetSize.width - 1, _g_widgCord.Y + 1, _g_widgCord.Y + WidgetSize.height - 1, false, tx_colr);
+			buffer->Set(SB_NEWBUFFER, _g_widgCord.X + WidgetSize.width - 1, _g_widgCord.Y + WidgetSize.height-1, Color::Pixel(BRS_CORNER, tx_colr));
+			buffer->LineHorisontal(_g_widgCord.Y + WidgetSize.height - 1, _g_widgCord.X + 1, _g_widgCord.X + WidgetSize.width - 1, false, tx_colr);
+			buffer->Set(SB_NEWBUFFER, _g_widgCord.X, _g_widgCord.Y + WidgetSize.height - 1, Color::Pixel(BLS_CORNER, tx_colr));
 		}
 
 		void Button::Click()
